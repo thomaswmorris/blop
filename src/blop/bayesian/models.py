4ad85524b9f603b73174b33fcb0477d8ad8a4278
Thomas Morris <thomas.w.morris@yale.edu>
2026-04-14 02:12:56 -0400

    add generation nodes

--- src/blop/bayesian/models.py
+++ src/blop/bayesian/models.py
@@ -1,23 +1,18 @@
-import warnings
 from typing import Any
 
-import botorch  # type: ignore[import-untyped]
 import gpytorch  # type: ignore[import-untyped]
 import torch  # type: ignore[import-untyped]
 from botorch.models.gp_regression import SingleTaskGP  # type: ignore[import-untyped]
 from botorch.models.multitask import MultiTaskGP  # type: ignore[import-untyped]
-from botorch.models.transforms.input import InputTransform, Normalize
-from botorch.models.transforms.outcome import OutcomeTransform, Standardize
+from botorch.models.transforms.input import InputTransform
+from botorch.models.transforms.outcome import OutcomeTransform
 from botorch.utils.types import DEFAULT, _DefaultType
 from gpytorch.likelihoods.likelihood import Likelihood
+from gpytorch.means import ConstantMean
+from gpytorch.priors import NormalPrior
 from torch import Tensor
 
 from . import kernels
-
-import gpytorch
-from gpytorch.priors import NormalPrior
-from gpytorch.means import ConstantMean
-from botorch.models.gp_regression import SingleTaskGP
 
 
 class LatentGP(SingleTaskGP):
@@ -34,18 +29,23 @@ class LatentGP(SingleTaskGP):
         *args: Any,
         **kwargs: Any,
     ) -> None:
-        
-        super().__init__(train_X=train_X,
-                         train_Y=train_Y, 
-                         input_transform=input_transform, 
-                         outcome_transform=outcome_transform,
-                         *args, **kwargs)
+
+        super().__init__(
+            train_X=train_X,
+            train_Y=train_Y,
+            input_transform=input_transform,
+            outcome_transform=outcome_transform,
+            *args,
+            **kwargs,
+        )
 
         m = train_Y.shape[-1]
         aug_batch_shape = train_X.shape[:-2] + (torch.Size([m]) if m > 1 else torch.Size())
 
         self.mean_module = ConstantMean(batch_shape=aug_batch_shape, constant_prior=NormalPrior(0.0, 1.0))
-        self.covar_module = kernels.RotatedInputsKernel(d=train_X.shape[-1], batch_shape=aug_batch_shape, skew_dims=skew_dims)
+        self.covar_module = kernels.RotatedInputsKernel(
+            d=train_X.shape[-1], batch_shape=aug_batch_shape, skew_dims=skew_dims
+        )
 
         # return SingleTaskGP(
         #     train_X=train_X,
@@ -55,7 +55,6 @@ class LatentGP(SingleTaskGP):
         #     outcome_transform=None,  # keep if you truly need it
         #     **kwargs,
         # )
-
 
 
 # class LatentGP(SingleTaskGP):
@@ -72,14 +71,14 @@ class LatentGP(SingleTaskGP):
 #         *args: Any,
 #         **kwargs: Any,
 #     ) -> None:
-        
+
 #         *batch_shape, n, d = train_X.shape
 #         input_transform = input_transform or Normalize(d=d)
 #         # outcome_transform = outcome_transform or Standardize(batch_shape=batch_shape)
 
 #         super().__init__(train_X=train_X,
-#                          train_Y=train_Y, 
-#                          input_transform=input_transform, 
+#                          train_Y=train_Y,
+#                          input_transform=input_transform,
 #                          outcome_transform=outcome_transform,
 #                          *args, **kwargs)
 
